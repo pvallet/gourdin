34fc8510ebfaf23a0f2330b4559765b2ab11882e
pvallet <luap.vallet@gmail.com>
2017-08-24 16:11:16 +0200

    Resolution is now chosen dynamically

--- src/main.cpp
+++ src/main.cpp
@@ -41,22 +41,19 @@ int main(int argc, char* argv[]) try {
   SDL2pp::SDLImage image(IMG_INIT_PNG);
 
 #ifdef __ANDROID__
-  SDL_DisplayMode displayMode;
-  SDL_GetDesktopDisplayMode(0, &displayMode);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-  SDL2pp::Window window("gourdin", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
-    SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
 #else
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-  SDL2pp::Window window("gourdin", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    1366, 768, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
 #endif
+
+  SDL_DisplayMode displayMode;
+  SDL_GetDesktopDisplayMode(0, &displayMode);
+
+  SDL2pp::Window window("gourdin", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, displayMode.w, displayMode.h,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
 
   glContext = SDL_GL_CreateContext(window.Get());
 
