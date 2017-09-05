17650a84c9efaf2d9ae4edab5854c76844cc49d4
pvallet <luap.vallet@gmail.com>
2017-09-05 09:20:34 +0200

    Fixed handling of condition variable

--- src/terrain/chunkSubdivider.cpp
+++ src/terrain/chunkSubdivider.cpp
@@ -6,29 +6,27 @@ ChunkSubdivider::ChunkSubdivider ():
   _computingThread(&ChunkSubdivider::executeTasks, this) {}
 
 void ChunkSubdivider::executeTasks() {
-  std::unique_lock<std::mutex> lock(_mutexCVQueueNotEmpty);
   while (true) {
+    std::unique_lock<std::mutex> lockQueue(_mutexQueue);
     while (_taskQueue.empty()) {
-      _cvQueueNotEmpty.wait(lock);
+      _cvQueueNotEmpty.wait(lockQueue);
     }
+    lockQueue.unlock();
 
     Task task = _taskQueue.front();
 
     task.chunk->generateSubdivisionLevel(task.subdivLvl);
 
-    _mutexQueue.lock();
+    lockQueue.lock();
     _taskQueue.pop();
-    _mutexQueue.unlock();
   }
 }
 
 void ChunkSubdivider::addTask(Chunk* chunk, size_t subdivLvl) {
-  _mutexQueue.lock();
+  std::unique_lock<std::mutex> lockQueue(_mutexQueue);
   _taskQueue.push({chunk, subdivLvl});
-  _mutexQueue.unlock();
 
   if (_taskQueue.size() == 1) {
-    std::unique_lock<std::mutex> lock(_mutexCVQueueNotEmpty);
     _cvQueueNotEmpty.notify_one();
   }
 }
--- src/terrain/chunkSubdivider.h
+++ src/terrain/chunkSubdivider.h
@@ -24,8 +24,7 @@ private:
   void executeTasks();
 
   std::condition_variable _cvQueueNotEmpty;
-  std::mutex _mutexCVQueueNotEmpty;
   std::mutex _mutexQueue;
-  std::thread _computingThread;
   std::queue<Task> _taskQueue;
+  std::thread _computingThread;
 };
